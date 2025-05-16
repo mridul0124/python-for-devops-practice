d936319fc3aec8bb398bde6f0bec9b74c6b4aec1
mridul0124 <panditmridul4@gmail.com>
2025-05-16 15:05:49 +0530

    Day-04 functions

---
+++ Day-04/calculator.py
@@ -0,0 +1,24 @@
+
+num1 = 10
+num2 = 5
+
+def add():
+    res = num1 + num2
+    print(res)
+    
+def subtract(): 
+    res = num1 - num2
+    print(res)              
+    
+def multiply():
+    res = num1 * num2
+    print(res)
+    
+def divide():
+    res = num1 // num2
+    print(res)
+    
+add()
+subtract()
+multiply()
+divide()
