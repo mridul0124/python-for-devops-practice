27c0b9d904d6ab77001df0250305b9b33689acd9
mridul0124 <panditmridul4@gmail.com>
2025-05-20 15:37:56 +0530

    command line args for calculator.py

--- Day-04/calculator.py
+++ Day-04/calculator.py
@@ -1,22 +1,33 @@
-
+import sys
 
 def add(num1, num2):
     res = num1 + num2
     return res
 
-def subtract(num1, num2): 
+def sub(num1, num2): 
     res = num1 - num2
     return res
 
-def multiply(num1, num2):
+def mul(num1, num2):
     res = num1 * num2
     return res
 
-def divide(num1, num2):
+def div(num1, num2):
     res = num1 // num2
     return res
 
-print(add(10, 5))
-print(subtract(10, 5))
-print(multiply(10, 5))
-print(divide(10, 5))
+
+num1 = float(sys.argv[1])
+operation = sys.argv[2]
+num2 = float(sys.argv[3])
+
+if operation == "add":
+    print(add(num1, num2))
+elif operation == "sub":
+    print(sub(num1, num2))
+elif operation == "mul":
+    print(mul(num1, num2))
+elif operation == "div":
+    print(div(num1, num2))
+else:
+    print("Invalid operation")
