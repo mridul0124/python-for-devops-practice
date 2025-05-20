02614d3bae0ee62edf302b410f8b2d4964767146
mridul0124 <panditmridul4@gmail.com>
2025-05-21 00:26:10 +0530

    Day-08 lists

---
+++ Day-08/test.py
@@ -0,0 +1,24 @@
+
+
+names = ["Alice", "Bob", "Charlie"]
+print(names[0])
+print(names)
+
+names[0] = "David"
+print(names[0])
+print(names)
+
+names.append("Eve")
+print(names)
+
+names.remove("Bob")
+print(names)
+
+names.insert(1, "Frank")
+print(names)
+
+names.pop()
+print(names)
+
+if "Charlie" in names:
+    print("Charlie is in the list")
