5ccb0e2d81063a4a997e30412901476d3cefd521
mridul0124 <panditmridul4@gmail.com>
2025-05-21 16:09:48 +0530

    Day-12 python file system

---
+++ Day-12/update_server.py
@@ -0,0 +1,15 @@
+
+
+def update_server_file(file_path,key,value):
+    
+    with open(file_path,'r') as file:
+        lines = file.readlines()
+        
+    with open(file_path,'w') as file:
+        for line in lines:
+            if key in line:
+                file.write(key+"="+value+"\n")
+            else:
+                file.write(line)
+                
+update_server_file("server.conf","PORT","8000")
