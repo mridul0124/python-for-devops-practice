46013f88358f4289b99d97052b221047c160a97f
mridul0124 <panditmridul4@gmail.com>
2025-05-21 12:00:53 +0530

    Day-09 loops in python

---
+++ Day-09/loops.py
@@ -0,0 +1,38 @@
+
+# ForLoop
+
+for i in range(10):
+    print("mridul")
+    
+    
+# ForLoop with else
+for i in range(10):
+    print("mridul")
+else:
+    print("Loop completed") 
+    
+# ForLoop with break
+for i in range(1,10):
+    if(i==7):
+        break
+    print(i)
+    
+# ForLoop with continue
+for i in range(10):
+    if(i==8):
+        continue
+    print(i)
+    
+# WhileLoop
+i = 0
+while(i<5):
+    print("mridul")
+    i+=1
+    
+# WhileLoop with else
+i = 0
+while(i<5):
+    print("mridul")
+    i+=1
+else:
+    print("Loop completed")
